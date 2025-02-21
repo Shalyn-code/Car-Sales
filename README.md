bee628acb33b6f3976ce4e300c476b56ad5fdeff
Shalyn-code <shalynlevron12@aol.com>
2025-02-21 03:39:29 -0800

    changes for render

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Car-Sales
+
+This project was created to compare listings of vehicles. At the end of the online service, there is a checkbox where the user can choose the type of vehicle, and it will display details about the vehicles in chosen category.
+
+Instructions to launch on a local machine:
+    1. Scan QR code below:
+    ![alt text][def]
+
+    If unsuccessful, follow this link: 
+        https://github.com/Shalyn-code/Car-Sales.git
+    and then fork into personal repository.
+
+
+
+[def]: image-2.png
+
+    2. Link Github repository to render and create new web service.
---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.header('Vehicles for Sale')
+
+st.write('Below is a histogram showing the amount of cars listed based on mileage:')
+
+fig = px.histogram(vehicles, x='odometer')
+
+fig.update_layout(
+    xaxis_range=[0, 500000],
+    xaxis_title='Mileage'
+)
+
+fig.show()
+
+st.write(fig)
+
+
+custom_colors = ['red', 'green', 'blue', 'yellow', 'coral']
+fig = px.scatter(vehicles, x ='model_year', y='price', color='type', color_discrete_sequence=custom_colors, size='days_listed')
+
+fig.update_layout(xaxis_title='Year of Vehicle')
+fig.show()
+
+event = st.plotly_chart(fig, key='type', on_select='rerun')
+event.selection
+
+others = ['pickup', 'coupe', 'hatchback', 'wagon', 'minivan', 'other', 'offroad', 'bus']
+sedan = st.checkbox('Sedan')
+suv = st.checkbox('SUV')
+truck = st.checkbox('Truck')
+van = st.checkbox('Van')
+convertible = st.checkbox('Convertible')
+
+# Filter DataFrame based on selected checkboxes
+filtered_df = pd.DataFrame()
+
+if sedan:
+    filtered_df = filtered_df.append(vehicles[vehicles['type'] == 'sedan'])
+
+if suv:
+    filtered_df = filtered_df.append(vehicles[vehicles['type'] == 'SUV'])
+
+if truck:
+    filtered_df = filtered_df.append(vehicles[vehicles['type'] == 'truck'])
+
+if pickup:
+    filtered_df = filtered_df.append(vehicles[vehicles['type'] == 'van'])
+
+if convertible:
+    filtered_df = filtered_df.append(vehicles[vehicles['type'] == 'convertible'])
+
+if others:
+    filtered_df = filtered_df.append(vehicles[vehicles['type'] == others])
+
+# Display the filtered DataFrame
+st.write(filtered_df[['price', 'model_year', 'model']])
