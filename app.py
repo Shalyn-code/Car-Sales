1297675a702bf12e42e0db0a7a382061d06e8a32
Shalyn-code <shalynlevron12@aol.com>
2025-03-06 06:04:16 -0800

    done

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-vehicles = pd.read_csv(r"C:\Users\shaly\.vscode\Car-Sales\vehicles_us.csv")
+vehicles = pd.read_csv('vehicles_us.csv')
 
 st.header('Vehicles for Sale')
 
